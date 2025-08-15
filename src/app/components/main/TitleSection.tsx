883db46e914161ff3b2757508fa0d3d7c0b13226
Yagiz <yagizdemirezen@gmail.com>
2025-08-16 01:32:18 +0300

    last changes really

--- src/app/components/main/TitleSection.tsx
+++ src/app/components/main/TitleSection.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image';
 import styles from '../../styles/TitleSection.module.css';
-import profileImage from '../../../../public/myPhoto1.jpg';
 import React from 'react';
 
 const TitleSection = React.forwardRef<HTMLDivElement>((props, ref) => {
   return (
     <div className={styles.mainContainer} ref={ref}>
       <h1 className={styles.title}>Yağız Demirezen | Jr. Fullstack Geliştirici</h1>
-      <Image className={styles.profileImage} src={profileImage} alt="Yağız Demirezen" width={200} height={200} />
+      <Image className={styles.profileImage} src='/myPhoto1.jpg' alt="Yağız Demirezen" width={200} height={200} />
     </div>
   );
 });
